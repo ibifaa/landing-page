7be0bbd665b7c6c523eeb059d60090477982c087
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-08-20 17:43:40 +0100

    new update

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -3,10 +3,10 @@ function TestimonialCard({ item }) {
   return (
     <div className="shadow-lg shadow-[#E6E6E6] w-[522px] h-[212px] p-5 rounded-[20px]">
   <div className="flex gap-4">
-    <div className="h-[72px] w-[72px] flex justify-center items-center bg-[#00C6C333] p-1 rounded-full">
+    <div className="h-[72px] w-[72px] flex justify-center items-center bg-[#00C6C333] p-1 rounded-full mb-[20px]">
       <img src={item.url} alt="icon" className="h-full w-full object-cover rounded-full" />
     </div>
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-center mg-[20px]">
       <h3 className="text-[18px] font-semibold leading-[24px]">
         {item.name}
       </h3>
