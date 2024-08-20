724f69a3a531e7bf2daefa24c2f8daf85cba4172
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-08-20 16:25:57 +0100

    first commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,47 +3,19 @@ import Partners from "../components/Partners";
 import { useEffect } from "react";
 
 function Hero() {
-  const slides = [
-    {
-      className: style.hero,
-      title: 'About Africa’s Women’s Day',
-      description: 'Celebrating African Women and creating more awareness about Africa’s Women’s Day!',
-      link: '/register',
-      buttonText: 'Read more'
-    },
-    {
-      className: style.hero2,
-      title: 'Celebrating Africa’s Women',
-      description: 'Let\'s keep creating awareness of African Women\'s Day, let us collectively recognize and honour the invaluable contribution of African Women',
-      link: '/register',
-      buttonText: 'Read more'
-    }
-  ];
-
-  const [currentSlide, setCurrentSlide] = useState(0)
-  const duration = 4000
-
-  useEffect(()=>{
-    const interval = setInterval(()=>{
-      setCurrentSlide((prevSlide) => (prevSlide+1) % slides.length)
-    }, duration);
-
-    return () => clearInterval(interval);
-  }, [slides.length])
-  
-    console.log(partners)
+ 
   return (
     <section   className=" hero">
         <div className="flex-wrap ">
             <div className="w-[100%] sm:w-[50%] sm:py-20 py-[20px]  px-5 sm:px-[100px]">
                 <h1 className="sm:text-[45px] text-[35px] font-extrabold text-white sm:leading-[72px] leading-[55px]">Empower Your Business With Our <span className="text-[#00C6C3]">Software</span>  Solutions</h1>
-                <p className="text-white text-[18px] leading-[27px] sm:my-[10px] my-[20px]">Simplify your operations with custom software designed to solve your unique business challenges</p>
-                <button className="text-[18px] px-[32px] py-[20px]  bg-[#00C6C3] w-[226px] h-[60px] rounded-[4px] sm:my-[10px] smy-[20px]">Learn More</button>
+                <p className="text-white text-[18px] leading-[27px] sm:my-[10px] my-[30px]">Simplify your operations with custom software designed to solve your unique business challenges</p>
+                <button className="text-[18px] px-[32px] py-[20px] my-[20px]  bg-[#00C6C3] w-[226px] h-[60px] rounded-[4px] sm:my-[10px] smy-[20px]">Learn More</button>
             </div>
             <div className="hidden sm:block"></div>
         </div>
         <div className="bg-[#52516580] sm:px-[100px] pb-[10px] px-[20px]">
-            <p className="text-white text-center pt-[10px]">Trusted by <span className="text-[#00C6C3]">50+ </span> Partners & Customers</p>
+            <p className="text-white text-center py-[20px]">Trusted by <span className="text-[#00C6C3]">50+ </span> Partners & Customers</p>
             <div className="grid sm:grid-cols-5 grid-cols-2 sm:mb-2 mb-[20px] ">
   {partners.map((item, index) => (
     <div key={index}>
