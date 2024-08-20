2bf146da639108a239005cdaa11b553f0248be4c
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-08-20 17:35:46 +0100

    update url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import facebook from "../assets/svg/facebook.svg"
+import x from "../assets/svg/x.svg";
+import linkedin from "../assets/svg/linkedin.svg";
 
 function Footer() {
   return (
@@ -9,48 +11,49 @@ function Footer() {
       <div className="grid sm:grid-cols-6 grid-cols-1">
         {" "}
         <div className="col-span-2">
-          <p>Creating cutting edge & future-proof digital solutions for you.</p>
-          <p>
+          <p className="text-[16px] leading-[24px] mb-[20px] w-[80%]">Creating cutting edge & future-proof digital solutions for you.</p>
+          <p className="text-[16px] leading-[24px] mb-[20px] w-[80%]">
             4A Etim Okpoyo Close, Furniture Square, after Total Filling Station
             By Aba Express Road, Port Harcourt, Rivers
           </p>
 
-          <div className="col-span-1">
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+          <div className="col-span-1 flex gap-[10px]">
+            <div className="bg-white h-[42px] w-[42px] rounded-full flex items-center justify-center"><img src={linkedin} alt="linkedin" /></div>
+            <div className="bg-white h-[42px] w-[42px] rounded-full flex items-center justify-center"><img src={x} alt="x" /></div>
+            <div className="bg-white h-[42px] w-[42px] rounded-full flex items-center justify-center"><img src={facebook} alt="facebook" /></div>
+           
           </div>
         </div>
         <div className="col-span-1">
           <h4 className="text-[20px] leading-[30px] font-bold mb-[20px]">Products</h4>
           <ul>
-            <li>Novia Care</li>
-            <li>Novia Book</li>
+            <li className="text-[16px] leacing-[24px]">Novia Care</li>
+            <li className="text-[16px] leacing-[24px]">Novia Book</li>
           </ul>
         </div>
         <div className="col-span-1">
           <h4 className="text-[20px] leading-[30px] font-bold mb-[20px]">Services</h4>
           <ul>
-            <li>Enterprise Resource Planning</li>
-            <li>Human Resource</li>
-            <li>Inventory Management</li>
-            <li>Ecommerce</li>
-            <li>Electronic Medical Record</li>
-            <li>Learning Management System</li>
+            <li className="text-[16px] leacing-[24px]">Enterprise Resource Planning</li>
+            <li className="text-[16px] leacing-[24px]">Human Resource</li>
+            <li className="text-[16px] leacing-[24px]">Inventory Management</li>
+            <li className="text-[16px] leacing-[24px]">Ecommerce</li>
+            <li className="text-[16px] leacing-[24px]">Electronic Medical Record</li>
+            <li className="text-[16px] leacing-[24px]">Learning Management System</li>
           </ul>
         </div>
         <div className="col-span-1">
           <h4 className="text-[20px] leading-[30px] font-bold mb-[20px]">Company</h4>
           <ul>
-            <li>About Us</li>
-            <li> Contact Us</li>
+            <li className="text-[16px] leacing-[24px]">About Us</li>
+            <li className="text-[16px] leacing-[24px]"> Contact Us</li>
           </ul>
         </div>
         <div className="col-span-1">
           <h4 className="text-[20px] leading-[30px] font-bold mb-[20px]">Legal</h4>
           <ul>
-            <li>Privacy Policy</li>
-            <li> Terms of Service</li>
+            <li className="text-[16px] leacing-[24px]">Privacy Policy</li>
+            <li className="text-[16px] leacing-[24px]"> Terms of Service</li>
           </ul>
         </div>
       </div>
