044a56789a5f89359b5b6994509e3239cc21c805
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-08-20 22:03:14 +0100

    udate on image using style

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,19 @@
 import partners from "../partner.js";
 import Partners from "../components/Partners";
 import { useEffect } from "react";
+import hero from "../assets/png/hero.png"
 
 function Hero() {
  
   return (
-    <section   className=" hero">
+    <section  className="
+  
+  "
+  style={{
+    backgroundImage: `linear-gradient(90deg, #020024CC 25%, rgba(188, 189, 190, 0.1) 40%), url(${hero})`,
+    // backgroundPosition: 'top left, -292px -199.998px',
+    // backgroundSize: 'cover, 120.893% 228.667%',
+  }}>
         <div className="flex-wrap ">
             <div className="w-[100%] sm:w-[50%] sm:py-20 py-[20px]  px-5 sm:px-[100px]">
                 <h1 className="sm:text-[45px] text-[35px] font-extrabold text-white sm:leading-[72px] leading-[55px]">Empower Your Business With Our <span className="text-[#00C6C3]">Software</span>  Solutions</h1>
